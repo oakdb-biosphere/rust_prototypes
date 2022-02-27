0cc4a6b9700375a43ebadc67c0707d0796b53ad0
craigfay <craigfay1@gmail.com>
2022-02-27 11:04:26 -0600

    Introduce clients collection

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,71 @@
 
 use std::{env, io::Error};
-use futures_util::{future, StreamExt, TryStreamExt};
+use std::collections::HashMap;
+use futures_util::{future, StreamExt, SinkExt, TryStreamExt};
 use tokio::net::{TcpListener, TcpStream};
+use tokio_tungstenite::{WebSocketStream};
+use tokio_tungstenite::tungstenite::Message;
 
+type ClientID = u64;
 
-fn main() {
-    let rt = tokio::runtime::Runtime::new().unwrap();
-    rt.block_on(serve_tcp()).unwrap();
+#[derive(Default)]
+pub struct DB {
+    next_client_id: ClientID,
 }
 
-async fn serve_tcp() -> Result<(), Error> {
+impl DB {
+    pub fn next_client_id(&mut self) -> ClientID {
+        self.next_client_id += 1;
+        self.next_client_id
+    }
+}
+
+type WSConnections = HashMap<ClientID, WebSocketStream<TcpStream>>;
+
+fn main() {
+    let db = DB::default();
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(expose_via_tcp(db)).unwrap();
+}
+
+async fn expose_via_tcp(mut db: DB) -> Result<(), Error> {
     let addr = env::args().nth(1).unwrap_or("127.0.0.1:8080".to_string());
     let tcp_socket = TcpListener::bind(&addr).await;
     let tcp_listener = tcp_socket.expect("Failed to bind");
     println!("Accepting TCP on: {}", addr);
 
+    let mut clients: WSConnections = HashMap::new();
+
     loop {
         match tcp_listener.accept().await {
-            Ok((tcp_stream, _)) => { tokio::spawn(accept_connection(tcp_stream)); },
             Err(e) => println!("Failed to accept connection: {}", e),
+            Ok((tcp_stream, _addr)) => {
+
+                let ws_stream = tokio_tungstenite::accept_async(tcp_stream).await
+                    .expect("Error during the websocket handshake occurred");
+
+                // Storing client in table
+                let client_id = db.next_client_id();
+                clients.insert(client_id, ws_stream);
+
+                let ws_stream = clients.get_mut(&client_id).unwrap();
+                let message = format!("Your cid is {}", client_id);
+
+                ws_stream.send(Message::Text(message)).await
+                    .expect("Could not send message");
+
+                // tokio::spawn(accept_connection(tcp_stream));
+            },
         }
     }
 }
 
 async fn accept_connection(stream: TcpStream) {
-    let addr = stream.peer_addr().expect("connected streams should have a peer address");
-    println!("Peer address: {}", addr);
-
-    let ws_stream = tokio_tungstenite::accept_async(stream)
-        .await
+    let ws_stream = tokio_tungstenite::accept_async(stream).await
         .expect("Error during the websocket handshake occurred");
 
-    println!("New WebSocket connection: {}", addr);
-
     let (write, read) = ws_stream.split();
-    // We should not forward messages other than text or binary.
+
     read.try_filter(|msg| future::ready(msg.is_text() || msg.is_binary()))
         .forward(write)
         .await
